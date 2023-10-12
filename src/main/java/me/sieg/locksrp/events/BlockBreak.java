f360fce4f9db7b207b43987bf25a26de5ffc073d
Felipe Roberto <56833987+feliperobertosouz@users.noreply.github.com>
2023-10-12 13:47:25 -0300

    feat: created MessageSender.java + updated BlockBreak to have less code lines and better semantic.

--- src/main/java/me/sieg/locksrp/events/BlockBreak.java
+++ src/main/java/me/sieg/locksrp/events/BlockBreak.java
@@ -18,51 +18,37 @@ public class BlockBreak implements Listener {
     public BlockBreak() {
     }
 
+    MessageSender sender = new MessageSender();
+    ItemManager itemManager = new ItemManager();
     @EventHandler
     public void onBreakBlock(BlockBreakEvent event) {
         Block block = event.getBlock();
         Player player = event.getPlayer();
-        // check if the clicked block is a door
-        if (SaveDoor.isDoor(block)) {
-            SaveDoor saveDoor = new SaveDoor();
+        SaveDoor saveDoor = new SaveDoor();
+        if ( block != null && SaveDoor.isDoor(block)) {
             Location loc = event.getBlock().getLocation();
-            //check if the door is registered
             Door doorDataDois = (Door) block.getBlockData();
             if(doorDataDois.getHalf() == Bisected.Half.TOP){
                 loc = SaveDoor.getBlockBelow(block.getLocation());
             }
             if (saveDoor.isLocationRegistered(loc)) {
-                //Checa se a porta esta trancada e o player não tem permissão
-                //Check if the door is locked or the player has permission
-            //System.out.println("QUEBRADO PORTA");
-                if (saveDoor.isDoorLocked(loc)) {
+                //System.out.println("QUEBRADO PORTA");
+                boolean  dorLocked = saveDoor.isDoorLocked(loc);
+                if (dorLocked) {
                     //System.out.println("PORTA TRANCADA");
                     event.setCancelled(true);
-                    player.sendMessage(ChatColor.RED + "A porta esta trancada, destranque antes de tirar");
-                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1.0f, 1.0f);
-                } else if (!saveDoor.isDoorLocked(loc)) {
+                    sender.sendPlayerMessage(player, "&cA porta esta trancada, destranque antes de tirar", Sound.ENTITY_VILLAGER_NO, 1.0f, 1.0f);
+                } else {
                     //System.out.println("PORTA DESTRANCADA");
-                    if (block instanceof Door) {
-                        Door doorData = (Door) block.getBlockData();
-                        // Verifica se é a parte de cima da porta
-                        //check if clicked block is the top of door
-                        if (doorData.getHalf() == Door.Half.TOP) {
-                            //get bottom door
-                            loc = SaveDoor.getBlockBelow(block.getLocation());
-
-                        }
-
-                        // Remove the locatio of doors.yml
-
-                    }
                     if(LandsChecker.PlayerCanBreakLand(player)) {
                         //System.out.println("PODE RETIRAR A TRANCA");
-                        ItemManager itemManager = new ItemManager();
+
                         String keyCode = saveDoor.getLockCode(loc);
                         Integer level = saveDoor.getLockLevel(loc);
                         ItemStack itemDrop = itemManager.generateLock(level, keyCode);
                         saveDoor.dropItemOnGround(loc, itemDrop);
                         player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
+                        sender.sendPlayerMessage(player, "Voce quebrou a porta que estava trancada", Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                         saveDoor.removeLocationFromFile(loc);
                     }else{
                         //System.out.println("NÃO PODE RETIRAR A TRANCA");
@@ -71,7 +57,7 @@ public class BlockBreak implements Listener {
                 }
             }
 
-        } else if (block != null && block.getType() == Material.CHEST
+        } else if (block.getType() == Material.CHEST
                 || block.getType() == Material.BARREL) {
             Inventory chestInventory = player.getInventory();
             if (block.getType() == Material.CHEST) {
@@ -91,10 +77,8 @@ public class BlockBreak implements Listener {
                     if (firstItem.hasItemMeta() && NameSpacedKeys.isLock(firstItem.getItemMeta())) {
                         if (NameSpacedKeys.hasKeyCode(firstItem.getItemMeta())) {
                             event.setCancelled(true);
-                            player.sendMessage(ChatColor.DARK_RED + "Tire a tranca do bau para o quebrar primeiro");
+                            sender.sendPlayerMessage(player, "&4Tire a tranca do bau para o quebrar primeiro", Sound.ENTITY_VILLAGER_NO, 1.0f, 1.0f);
                         }
-
-
                     }
                 }
             }
